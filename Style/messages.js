c36749bcd9c82ba13ddb8d3cdf80c6ebfc46845f
malak2004malak000004-lab <malak2004malak000004@gmail.com>
2025-11-29 00:39:17 +0300

    Delete Style directory

--- Style/messages.js
+++
@@ -1,9 +0,0 @@
-const stars = document.querySelectorAll('.message-star');
-const messageBox = document.getElementById('message-text');
-
-stars.forEach(star => {
-    star.addEventListener('click', () => {
-        messageBox.textContent = star.getAttribute('data-msg');
-        messageBox.classList.add('show');
-    });
-});
